db63f88c4bd5319029a88c52c762340fa6da7cf1
Darya Tarakanovskaya <d.tarakanovskaya@gmail.com>
2022-10-14 11:20:16 +0700

    final v2

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -9,7 +9,7 @@ def game_progression():
     value_2 = randrange(1, 11)
     array = [*range(value_1, value_1 + 10 * value_2, value_2)]
     index = randrange(10)
-    v = f"{' '.join(str(w) for w in array[:index])} ... {' '.join(str(w) for w in array[index+1:])}"
+    v = f"{' '.join(str(w) for w in array[:index])} .. {' '.join(str(w) for w in array[index+1:])}"
     real_answer = array[index]
     print(v)
     get_answer = prompt.string(f'Your answer: ')
